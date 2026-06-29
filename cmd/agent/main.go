5acf5f2fb390282551ec62f3ad56607715af4c03
PArk <evilsizemoney@mail.ru>
2026-06-29 12:46:24 +0300

    add agent entry point in main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,10 +1,34 @@
 package main
 
-import "runtime"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/76Parker/metrico/internal/agent/provider"
+	"github.com/76Parker/metrico/internal/agent/reporter"
+)
+
+const (
+	pollInterval   = 2 * time.Second
+	reportInterval = 10 * time.Second
+)
 
 func main() {
 
-	metrics := runtime.MemStats{}
-	runtime.ReadMemStats(&metrics)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
+	httpClient := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	serverAddr := "http://localhost:8080"
+	provider := provider.NewMetricProvider(pollInterval)
+	reporter := reporter.NewMetricReporter(serverAddr, httpClient, provider, reportInterval)
+	if err := reporter.Run(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
